c4ff8f797f079e6b9a84e152bf53fb3587574695
Mariana Bernardes <mari.bernardes93@gmail.com>
2016-09-08 13:30:35 -0300

    Add projeto example-fixwo-easybpms

---
+++ example-fixwo-easybpms/src/main/java/org/fixwo/dao/OcorrenciaDAO.java
@@ -0,0 +1,17 @@
+package org.fixwo.dao;
+
+import org.fixwo.domain.Ocorrencia;
+
+import com.easybpms.event.CRUDObservable;
+
+public class OcorrenciaDAO extends CRUDObservable{
+	
+	public void create (Ocorrencia o){
+		notifyObservers(o);
+	}
+	
+	public void update (Ocorrencia o){
+		notifyObservers(o);
+	}
+
+}
